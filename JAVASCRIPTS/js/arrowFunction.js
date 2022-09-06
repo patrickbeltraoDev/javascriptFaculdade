ca8acdc6c45f58fa56023930cb9656b0606b6f3e
Patrick Thiago Martins Beltrao <patrick.beltrao@RGOLOC-BRJ218XYGZ.telemont.net>
2022-09-06 11:33:38 -0300

    d

--- JAVASCRIPTS/js/arrowFunction.js
+++ JAVASCRIPTS/js/arrowFunction.js
@@ -2,5 +2,10 @@
 var cores = [
     {'id': 1, 'nome': 'Amarelo'},
     {'id': 2, 'nome': 'Preto'},
-    {'id': 3, 'nome': 'Branco'}
+    {'id': 3, 'nome': 'Branco'},
 ];
+
+const coresNomes = cores.map(cor => cor.nome);
+
+var resultado = document.getElementById('result');
+resultado.innerHTML += coresNomes;
